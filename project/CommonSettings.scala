3b7f7a1dd0118ead4db3669ecf63f36de52dac57
ndubina <ndubina@emdev.ru>
2016-08-04 17:18:06 +0600

    fix for liferay support version

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -38,7 +38,7 @@ object Settings {
 
   object Liferay620 {
     val dependencies = Dependencies.liferay62
-    val supportVersion = "6.2.5"
+    val supportVersion = "6.2.*"
     val version = Version.liferayPortal62
   }
 
